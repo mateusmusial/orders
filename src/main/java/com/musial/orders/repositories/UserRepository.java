8e76b12157d4f6bbe45a127640d3534c8cb1799d
Mateus Musial <mateus.musial@gmail.com>
2024-01-05 22:18:44 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/musial/orders/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.musial.orders.repositories;
+
+import com.musial.orders.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
