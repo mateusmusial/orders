7570cdb1c82481f1c1bad38f10ceeb4b66cca887
Mateus Musial <mateus.musial@gmail.com>
2024-01-05 22:51:40 -0300

    Service layer, component registration

--- src/main/java/com/musial/orders/resources/UserResource.java
+++ src/main/java/com/musial/orders/resources/UserResource.java
@@ -1,18 +1,32 @@
 package com.musial.orders.resources;
 
 import com.musial.orders.entities.User;
+import com.musial.orders.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "Mateus", "mateus@gmail.com", "98989-8909");
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
         return ResponseEntity.ok().body(user);
     }
 }
