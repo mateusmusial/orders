6d49bd0a9f053ce03a8768959e8f46e28f068d03
Mateus Musial <mateus.musial@gmail.com>
2024-01-05 21:49:52 -0300

    H2 database, test profile, JPA

--- src/main/java/com/musial/orders/entities/User.java
+++ src/main/java/com/musial/orders/entities/User.java
@@ -1,12 +1,18 @@
 package com.musial.orders.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 
+@Entity
+@Table(name = "users")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
