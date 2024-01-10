32abbb50601a5c51c96cec9564163c97e7bd8373
Mateus Musial <mateus.musial@gmail.com>
2024-01-09 21:37:51 -0300

    OrderStatus enum

--- src/main/java/com/musial/orders/entities/Order.java
+++ src/main/java/com/musial/orders/entities/Order.java
@@ -2,6 +2,7 @@ package com.musial.orders.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.musial.orders.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +24,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -30,9 +33,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStaus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStaus);
         this.client = client;
     }
 
@@ -54,6 +58,16 @@ public class Order implements Serializable {
 
     public User getClient() {
         return client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public void setClient(User client) {
