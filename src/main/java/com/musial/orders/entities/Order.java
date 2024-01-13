e4c6088372e842fb84bf9228b0180364392865ef
Mateus Musial <mateus.musial@gmail.com>
2024-01-13 17:33:43 -0300

    Payment, one-to-one association

--- src/main/java/com/musial/orders/entities/Order.java
+++ src/main/java/com/musial/orders/entities/Order.java
@@ -39,6 +39,12 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(
+            mappedBy = "order",
+            cascade = CascadeType.ALL
+    )
+    private Payment payment;
+
     public Order() {
     }
 
@@ -85,6 +91,10 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
